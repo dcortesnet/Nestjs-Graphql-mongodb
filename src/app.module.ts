aef91dc7b07a5f32aa3d5ba95e0887abee10a9c3
dcortesnet <dcortes.net@gmail.com>
2023-12-21 22:54:18 -0300

    Resolvers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Author, AuthorSchema } from './schemas/author.schema';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { AppResolver } from './app.resolver';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/test'),
     MongooseModule.forFeature([{ name: Author.name, schema: AuthorSchema }]),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
